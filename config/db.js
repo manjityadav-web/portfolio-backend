8b0738c79ec0c2de471e26b6b7bc25b981e52d51
Manjit Yadav <manjityadav9986@gmail.com>
2025-08-24 23:51:49 +0530

    changing database url

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 
 export const dbConnect = async ()=>{
-   const connect =  await mongoose.connect("mongodb://localhost:27017/portfolio")
+   const connect =  await mongoose.connect("mongodb+srv://manjityadav9986:BYkgs67j8Xp7lpX6@cluster1.dsdlm18.mongodb.net/portfolio")
+
    if(connect) {
       console.log("Database connected successfully")
    }
